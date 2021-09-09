92163640c48810c93ff5ad46997e44a00991f54a
Adi Skop <adiskop@gmail.com>
2021-09-09 10:44:22 +0200

    add componentdidmount and connect to render login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
-import { render } from '@testing-library/react';
 import React from 'react';
+import Login from "./components/Login.js"
+import { connect } from 'react-redux'
+import { getCurrentUser } from "./actions/currentUser.js"
 
 class App extends React.Component {
+
+  componentDidMount() {
+    this.props.getCurrentUser()
+  }
+
   render(){
     return (
-      "this is React"
+      <Login />
     );
   }
 }
 
-export default App;
+export default connect(null, { getCurrentUser }) (App);
