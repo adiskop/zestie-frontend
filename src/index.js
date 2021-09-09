d9c247483411c8ee913959a1412e46f620627f1d
Adi Skop <adiskop@gmail.com>
2021-09-09 10:45:44 +0200

    delete some imports

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import store from './store.js'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={ store }><App /></Provider>,
   document.getElementById('root')
 );
 
