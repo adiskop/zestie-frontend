2bf78b839863108509a0f69ab5e2fb55cfc45637
Adi Skop <adiskop@gmail.com>
2021-09-11 15:09:33 +0200

    add NavBar back and Logout

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import MainContainer from './components/MainContainer';
 import NavBar from './components/NavBar';
 import Login from './components/Login';
 import Logout from './components/Logout';
+import Signup from './components/Signup.js';
 import MyDishes from './components/MyDishes';
 import { Router } from 'react-router';
 import { BrowserRouter, Route } from 'react-router-dom' 
@@ -21,9 +22,10 @@ class App extends React.Component {
     return (
       <BrowserRouter>
       <div className="App">
-      <NavBar/>
+        <Logout/>
+        <NavBar/>
+          <Route exact path='/signup' component ={Signup}/>
           <Route exact path='/login' component ={Login}/>
-          <Route exact path='/logout' component ={Logout}/>
           <Route exact path='/my-dishes' component ={MyDishes}/>
       </div>
       </BrowserRouter>
