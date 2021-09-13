e344facb0de005053425b8767d453a3f85cea6be
Adi Skop <adiskop@gmail.com>
2021-09-13 12:04:20 +0200

    add NavBar back into homepage if logged in

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import './App.css';
 import { connect } from 'react-redux'
 import { getCurrentUser } from "./actions/currentUser.js"
-
+import NavBar from './components/NavBar';
 import Home from './components/Home';
 import Login from './components/Login';
 import Logout from './components/Logout';
 import Signup from './components/Signup.js';
 import MyDishes from './components/MyDishes';
 import NewDishForm from './components/NewDishForm';
-
 import { Switch, Route, withRouter } from 'react-router-dom' 
 
 
@@ -22,14 +21,11 @@ class App extends React.Component {
   render(){
     const { loggedIn } = this.props
     return (
-     
       <div className="App">
-        {loggedIn ? <Logout/> : null}
+        {loggedIn ? <NavBar/> : <Home/> }
         <Switch>
           <Route exact path='/signup' component ={Signup}/>
           <Route exact path='/login' component ={Login}/>
-          <Route exact path='/' render={(props)=> loggedIn ? <MyDishes
-          {...props}/> : <Home {...props}/>}/>
           <Route exact path='/dishes' component ={MyDishes}/>
           <Route exact path='/dishes/new' component ={NewDishForm}/>
           </Switch>
