a69a69856f4e9e617b903dbc891af7575b7e30b1
Adi Skop <adiskop@gmail.com>
2021-09-13 12:08:10 +0200

    fix links

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,9 @@ import { Link } from "react-router-dom";
 
 const Home = ({}) => (
     <div>
-    <h4>Welcome, please <Link to="/signup">Sign Up</Link> 
+    <h4>Welcome, please <Link to="/signup">Sign Up  </Link> 
     OR 
-    <Link to="/login">Log In</Link></h4>
+    <Link to="/login">  Log In</Link></h4>
     </div>
 );
    
