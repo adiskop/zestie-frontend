9b842c8e3e9f27e119e0fb26c339c1abff84d3d3
Adi Skop <adiskop@gmail.com>
2021-09-13 12:06:10 +0200

    add clear dishes reducer

--- src/reducers/myDishes.js
+++ src/reducers/myDishes.js
@@ -1,7 +1,11 @@
+const initialState = []
+
 export default (state = [], action) => {
     switch (action.type) {
       case "SET_MY_DISHES":
         return action.dishes
+      case "CLEAR_DISHES":
+      return initialState
       default:
         return state
     }
