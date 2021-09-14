85ecb4c6659ed6e5704466d9464fd32aac7e0289
Adi Skop <adiskop@gmail.com>
2021-09-14 15:16:59 +0200

    add img tag

--- src/components/DishCard.js
+++ src/components/DishCard.js
@@ -6,7 +6,10 @@ const DishCard = ({ dish }) => {
         dish?
         <div>
             <h3>{dish.attributes.name}</h3>
-            <p>{dish.attributes.picture}</p>
+                <img src ={dish.attributes.picture} className="dish-picture"/>
+                <p>{dish.attributes.ingredients}</p>
+                <p>{dish.attributes.directions}</p>
+                <p>{dish.attributes.cook_time}</p>
             </div> :
       <p>This the the Dish card with no dish!</p>
     )
