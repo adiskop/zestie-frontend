7634a655cf66617c55b79ef19dcbb3827ec31c74
Adi Skop <adiskop@gmail.com>
2021-09-15 14:23:58 +0200

    add edit form action

--- src/actions/dishForm.js
+++ src/actions/dishForm.js
@@ -13,3 +13,19 @@ export const resetDishForm = () => {
       type: "RESET_DISH_FORM"
     }
   }
+
+
+export const setFormDataForEdit = dish => {
+  const dishFormData = {
+    name: dish.attributes.name,
+    picture: dish.attributes.picture,
+    ingredients: dish.attributes.ingredients,
+    directions: dish.attributes.directions,
+    cookTime: dish.attributes.cook_time
+  }
+
+  return {
+    type: "SET_FORM_DATA_FOR_EDIT",
+    dishFormData
+  }
+}
