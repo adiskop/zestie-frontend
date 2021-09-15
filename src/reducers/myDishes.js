36ad14f9a39d6032ea53e5a467966f1c963178bb
Adi Skop <adiskop@gmail.com>
2021-09-15 14:31:59 +0200

    add delete dish and clear reducers

--- src/reducers/myDishes.js
+++ src/reducers/myDishes.js
@@ -1,13 +1,17 @@
 const initialState = []
 
-export default (state = [], action) => {
+export default (state = initialState, action) => {
     switch (action.type) {
       case "SET_MY_DISHES":
         return action.dishes
-      case "CLEAR_DISHES":
-      return initialState
       case "ADD_DISH":
-      return state.concat(action.dish)
+        return state.concat(action.dish)
+      case "UPDATE_DISH":
+        return state.map(dish => dish.id === action.dish.id ? action.dish : dish)
+      case "DELETE_DISH":
+        return state.filter(dish => dish.id === action.dishId ? false : true)
+      case "CLEAR_DISHES":
+        return initialState
       default:
         return state
     }
