c1fe707630c50f80e46dc0ea935619c1190122d6
Adi Skop <adiskop@gmail.com>
2021-09-15 14:29:31 +0200

    fix typo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,11 @@
 import React from "react";
-import Login from "./Login";
-import Signup from "./Signup";
 import { Link } from "react-router-dom";
 
-const Home = ({}) => (
+const Home = () => (
     <div>
-    <h4>Welcome, please <Link to="/signup">Sign Up  </Link> 
+    <h4>Welcome, please <Link to="/signup">Sign Up</Link> 
     OR 
-    <Link to="/login">  Log In</Link></h4>
+    <Link to="/login">Log In</Link></h4>
     </div>
 );
    
