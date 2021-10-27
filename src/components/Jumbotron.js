26b65e1d4db8f48200bd34e8d66c3a2832c56699
Adi Skop <adiskop@gmail.com>
2021-10-27 21:45:52 +0200

    change typo

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -44,3 +44,7 @@ export const Jumbotron = () => (
     </Jumbo>
   </Styles>
 )
+
+
+
+export default Jumbotron;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 
 
-export const Layout = (props) => (
+const Layout = (props) => (
     
     <Container>
           {props.children}
@@ -10,4 +10,5 @@ export const Layout = (props) => (
   
 )
 
+export default Layout;
 
